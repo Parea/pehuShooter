d7b4d3ba5bd8ced60389a20cc07dc68c0befa027
Parea <taie.taataparea1@gmail.com>
2018-11-12 11:36:28 -1000

    Main scene finish

---
+++ Assets/_Complete-Game/Scripts/mainSceneService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainSceneService : MonoBehaviour {
+    public void LoadOnClick(int level) {
+        SceneManager.LoadScene(level);
+    }
+}
