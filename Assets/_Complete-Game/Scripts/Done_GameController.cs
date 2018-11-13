49f63616ac9deae9b5c2d393ae7bb74d014a114a
Parea <taie.taataparea1@gmail.com>
2018-11-13 13:27:17 -1000

    last update mainScene and others config

--- Assets/_Complete-Game/Scripts/Done_GameController.cs
+++ Assets/_Complete-Game/Scripts/Done_GameController.cs
@@ -1,7 +1,93 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
+﻿//using UnityEngine;
+//using UnityEngine.SceneManagement;
+//using System.Collections;
+//using UnityEngine.UI;
+
+//public class Done_GameController : MonoBehaviour
+//{
+//    public GameObject[] hazards;
+//    public Vector3 spawnValues;
+//    public int hazardCount;
+//    public float spawnWait;
+//    public float startWait;
+//    public float waveWait;
+
+//    public Text scoreText;
+//    public Text restartText;
+//    public Text gameOverText;
+
+//    private bool gameOver;
+//    private bool restart;
+//    private int score;
+
+//    void Start()
+//    {
+//        gameOver = false;
+//        restart = false;
+//        restartText.text = "";
+//        gameOverText.text = "";
+//        score = 0;
+//        UpdateScore();
+//        StartCoroutine(SpawnWaves());
+//    }
+
+//    void Update()
+//    {
+//        if (restart)
+//        {
+//            if (Input.GetKeyDown(KeyCode.R))
+//            {
+//                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+//            }
+//        }
+//    }
+
+//    IEnumerator SpawnWaves()
+//    {
+//        yield return new WaitForSeconds(startWait);
+//        while (true)
+//        {
+//            for (int i = 0; i < hazardCount; i++)
+//            {
+//                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+//                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+//                Quaternion spawnRotation = Quaternion.identity;
+//                Instantiate(hazard, spawnPosition, spawnRotation);
+//                yield return new WaitForSeconds(spawnWait);
+//            }
+//            yield return new WaitForSeconds(waveWait);
+
+//            if (gameOver)
+//            {
+//                restartText.text = "Press 'R' for Restart";
+//                restart = true;
+//                break;
+//            }
+//        }
+//    }
+
+//    public void AddScore(int newScoreValue)
+//    {
+//        score += newScoreValue;
+//        UpdateScore();
+//    }
+
+//    void UpdateScore()
+//    {
+//        scoreText.text = "Score: " + score;
+//    }
+
+//    public void GameOver()
+//    {
+//        gameOverText.text = "Perdu champion!";
+//        gameOver = true;
+//    }
+//}
+
+using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Done_GameController : MonoBehaviour
 {
@@ -13,34 +99,36 @@ public class Done_GameController : MonoBehaviour
     public float waveWait;
 
     public Text scoreText;
-    public Text restartText;
+    //  public Text restartText;
     public Text gameOverText;
+    public GameObject restartButton;
 
     private bool gameOver;
-    private bool restart;
+    //  private bool restart;
     private int score;
 
     void Start()
     {
         gameOver = false;
-        restart = false;
-        restartText.text = "";
+        //      restart = false;
+        //      restartText.text = "";
         gameOverText.text = "";
+        restartButton.SetActive(false);
         score = 0;
         UpdateScore();
         StartCoroutine(SpawnWaves());
     }
 
-    void Update()
-    {
-        if (restart)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
+    //  void Update ()
+    //  {
+    //      if (restart)
+    //      {
+    //          if (Input.GetKeyDown (KeyCode.R))
+    //          {
+    //              Application.LoadLevel (Application.loadedLevel);
+    //          }
+    //      }
+    //  }
 
     IEnumerator SpawnWaves()
     {
@@ -59,8 +147,9 @@ public class Done_GameController : MonoBehaviour
 
             if (gameOver)
             {
-                restartText.text = "Press 'R' for Restart";
-                restart = true;
+                restartButton.SetActive(true);
+                //              restartText.text = "Press 'R' for Restart";
+                //              restart = true;
                 break;
             }
         }
@@ -79,7 +168,17 @@ public class Done_GameController : MonoBehaviour
 
     public void GameOver()
     {
-        gameOverText.text = "Game Over!";
+        gameOverText.text = "Perdu champion!";
         gameOver = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadOnClick(int level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
--- Assets/_Complete-Game/Scripts/Done_PlayerController.cs
+++ Assets/_Complete-Game/Scripts/Done_PlayerController.cs
@@ -1,49 +1,140 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+
+//[System.Serializable]
+//public class Done_Boundary
+//{
+//    public float xMin, xMax, zMin, zMax;
+//}
+
+//public class Done_PlayerController : MonoBehaviour
+//{
+//    public float speed;
+//    public float tilt;
+//    public Done_Boundary boundary;
+
+//    public GameObject shot;
+//    public Transform shotSpawn;
+//    public float fireRate;
+
+//    private float nextFire;
+
+//    void Update()
+//    {
+//        if (Input.GetButton("Fire1") && Time.time > nextFire)
+//        {
+//            nextFire = Time.time + fireRate;
+//            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+//            GetComponent<AudioSource>().Play();
+//        }
+//    }
+
+//    void FixedUpdate()
+//    {
+//        float moveHorizontal = Input.GetAxis("Horizontal");
+//        float moveVertical = Input.GetAxis("Vertical");
+
+//        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+//        GetComponent<Rigidbody>().velocity = movement * speed;
+
+//        GetComponent<Rigidbody>().position = new Vector3
+//        (
+//            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+//            0.0f,
+//            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+//        );
+
+//        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+//    }
+//}
+
+///////////////////////////////////////////////Code adaptée au mobile//////////////////////////////////////////////////////////////////
+
+using UnityEngine;
 using System.Collections;
 
 [System.Serializable]
-public class Done_Boundary 
+public class Done_Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
 public class Done_PlayerController : MonoBehaviour
 {
-	public float speed;
-	public float tilt;
-	public Done_Boundary boundary;
+    public float speed;
+    public float tilt;
+    public Done_Boundary boundary;
 
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-	 
-	private float nextFire;
-	
-	void Update ()
-	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
-		}
-	}
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public SimpleTouchPad touchPad;
+    public SimpleTouchAreaButton areaButton;
 
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    private float nextFire;
+    private Quaternion calibrationQuaternion;
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
-			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
-	}
+    void Start()
+    {
+        CalibrateAccelerometer();
+    }
+
+    void Update() //Update avec les simpleTouch
+    {
+        if (areaButton.CanFire() && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetButton("Fire1") && Time.time > nextFire)
+    //    {
+    //        nextFire = Time.time + fireRate;
+    //        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+    //        GetComponent<AudioSource>().Play();
+    //    }
+    //}
+
+    //Used to calibrate the Iput.acceleration input
+    void CalibrateAccelerometer()
+    {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    //Get the 'calibrated' value from the Input
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration;
+    }
+
+    void FixedUpdate()
+    {
+        ////////////////////////////////////////Si ont veut utiliser le gyroscope pour jouer////////////////////////////////////////////////////
+
+        //Vector3 accelerationRaw = Input.acceleration;
+        //Vector3 acceleration = FixAcceleration(accelerationRaw);
+        //Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+
+        ////////////////////////////////////////Si ont veut utiliser les touchPad pour jouer////////////////////////////////////////////////////
+
+        Vector2 direction = touchPad.GetDirection();
+        Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        );
+
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
 }
